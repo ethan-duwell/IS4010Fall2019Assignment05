ddbc2fc8ab9427a38fbdde8e68953147f8bdc733
duwellen <duwellen@LCBLAB2110-12.uccob.uc.edu>
2019-09-26 09:30:08 -0400

    Created a method that takes a football score and returns a string describing that score Fixed program to return "All tests passed"

--- IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
+++ IS4010Fall2019Assignment05/src/footballFun/FootballFun.java
@@ -1,14 +1,17 @@
 /*
- * IS4010 Fal 2019
+ * IS4010 Fall 2019
  * Assignment 05
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
+ * Ethan Duwell
+ * duwellen@mail.uc.edu
  */
 package footballFun;
+
+import java.util.Scanner;
 
 public class FootballFun {
 
 	public static void main(String[] args) {
+
 		int correctCount = 0;
 		if (translateScore(2).equals("safety") == true) { correctCount++;}
 		if (translateScore(3).equals("field goal") == true) { correctCount++;}
@@ -25,7 +28,39 @@ public class FootballFun {
 			System.out.println("At least one test failed");
 		}
 	}
+	/**
+	 * We created a method that takes a football score and returns a string describing that score
+	 * @param score
+	 * @return "All tests passed"
+	 */
 	public static String translateScore (int score) {
-		return "";
+		if (score == 2) {
+			return "safety";
+		}
+		else if (score == 3) {
+			return "field goal";
+		}
+		else if (score == 6) {
+			return "touchdown";
+		}
+		else if (score == 7) {
+			return "touchdown and extra point";
+		}
+		else if (score == 8) {
+			return "touchdown and 2-point conversion";
+		}
+		else if (score < 0) {
+			return "";
+		}
+		else if (score == 1) {
+			return "invalid";
+		}
+		else if (score == 10) {
+			return "you must be playing Quidditch ";
+		}
+		else {
+			return "";
+		}
+
 	}
 }
